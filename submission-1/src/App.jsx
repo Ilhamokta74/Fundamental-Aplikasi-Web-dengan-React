3a65b5d683b5167dbdcb2e7e957af499931c897f
Ilham Oktavian <99379666+Ilhamokta74@users.noreply.github.com>
2024-08-11 20:24:12 +0700

    updated App.jsx

--- submission-1/src/App.jsx
+++ submission-1/src/App.jsx
@@ -3,9 +3,8 @@ import Header from "./components/Header"
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Archived from "./pages/Archived"
-import DetailNote from "./pages/DetailNote"
 import AddNote from "./pages/AddNote"
-import PageNotFount from "./pages/404"
+import PageNotFound from "./pages/404"
 
 class App extends React.Component {
 	render() {
@@ -15,9 +14,8 @@ class App extends React.Component {
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/archives" element={<Archived />} />
-					<Route path="/detail/:id" element={<DetailNote />} />
 					<Route path="/notes/new" element={<AddNote />} />
-					<Route path="/*" element={<PageNotFount />} />
+					<Route path="/*" element={<PageNotFound />} />
 				</Routes>
 			</>
 		)
